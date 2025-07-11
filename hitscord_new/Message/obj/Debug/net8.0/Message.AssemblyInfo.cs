47d67b9ee13a61ad4a277afc4b0d90d7e10c3ba9
Pyotr <mistiquelico@gmail.com>
2025-07-11 13:22:06 +0700

    Изменил максимальное количество

--- hitscord_new/Message/obj/Debug/net8.0/Message.AssemblyInfo.cs
+++ hitscord_new/Message/obj/Debug/net8.0/Message.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Message")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673fcc3771f21f3a1e5c4448a962ce38ad905c5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4d4a2e3596f40a9f68ba4095187a5722764c89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Message")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Message")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
