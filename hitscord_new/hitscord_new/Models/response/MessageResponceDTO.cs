a965d742ebdaec1cfb8a9a8031803d72189a5e41
Pyotr <mistiquelico@gmail.com>
2025-10-23 10:40:05 +0700

    Дописал состав уведомления

--- hitscord_new/hitscord_new/Models/response/MessageResponceDTO.cs
+++ hitscord_new/hitscord_new/Models/response/MessageResponceDTO.cs
@@ -5,8 +5,8 @@ public class MessageResponceDTO
     public required string MessageType { get; set; }
 	public Guid? ServerId { get; set; }
 	public string? ServerName { get; set; }
-	public required Guid ChannelId { get; set; }
-	public required string ChannelName { get; set; }
+	public Guid? ChannelId { get; set; }
+	public string? ChannelName { get; set; }
 	public required long Id { get; set; }
     public required Guid AuthorId { get; set; }
     public required DateTime CreatedAt { get; set; }
--- hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
+++ hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1508fa21daa760d4354810ea5db4eaa4baae4ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece1e8eb18cdf96b95e9653a7e92443bf8ebb58b")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
