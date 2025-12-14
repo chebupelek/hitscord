b99d3d7f68714bc2826072f14e20c9aeae42f6b2
Pyotr <mistiquelico@gmail.com>
2025-12-14 15:39:05 +0700

    Переделал запрос подтверждения заявки

--- hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
+++ hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5be95c12e95f020a5a0292f6e1e8869ffed92db8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+365fb2710489d7685fcb7e5859346ec794d75fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
