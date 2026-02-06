842eb17e93a3dd837e96dffce9280b450c6d03fc
Pyotr <mistiquelico@gmail.com>
2026-02-07 00:33:14 +0700

    Operation history

--- hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
+++ hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+536ab19120144e1d4f53b3acc29c11404bb80426")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744792f12e271b0de48d6dcc1d08b47810b87437")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
