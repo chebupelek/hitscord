7319b5392b19083c172dfd4f898f5c622f5cb583
Pyotr <mistiquelico@gmail.com>
2026-02-11 20:31:41 +0700

    no cors 2

--- hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
+++ hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+744792f12e271b0de48d6dcc1d08b47810b87437")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df919320a6e06d135e1774d71d7f680e18ac8aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
