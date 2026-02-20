a3dfb2f3dc8bfb531013bb1c6ecda335b906b900
Pyotr <mistiquelico@gmail.com>
2026-02-21 02:26:30 +0700

    Подгрузка сообщений

--- hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
+++ hitscord_new/hitscord_new/obj/Debug/net8.0/hitscord_new.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5df919320a6e06d135e1774d71d7f680e18ac8aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f27feee86fa0d02bea2fb2aba0b1f07c128ff41")]
 [assembly: System.Reflection.AssemblyProductAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hitscord_new")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
